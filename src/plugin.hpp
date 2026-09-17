9e75f492181002f7a485fad5c1309e6e7240b0ac
Skarmuse <98064124+BooleanMuse@users.noreply.github.com>
2026-09-17 15:08:52 -0600

    delete old files

--- src/plugin.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <rack.hpp>
-
-using namespace rack;
-
-extern Plugin* pluginInstance;
-extern Model* modelFunkinOpponent;
-extern Model* modelFunkinPlayer;
-extern Model* modelFunkinDancer;
